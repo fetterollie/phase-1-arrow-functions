61065082329ee5d66cbd176d9f0fa0b755900442
Jonathan <jonathan.fetterolf@gmail.com>
2023-05-18 14:38:54 -0400

    functions complete and passing tests

---
+++ index.js
@@ -0,0 +1,11 @@
+    const divide = (num1, num2) => num1 / num2
+    // 1) has a function expression called divide
+    // 2) divide divides 2000 by 100
+
+    const square = num => num ** 2
+    // 3) has an arrow function called square
+    // 4) square arrow function takes one parameter and multiplies it times itself
+
+    const add = (num1, num2) => num1 + num2
+    // 5) has an arrow function called add
+    // 6) add arrow function takes two parameters and adds them together
